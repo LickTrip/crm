0194c1af420669ec5659cff98f6bf9ea4c70dca9
Michal <lick.trip@gmail.com>
2019-08-06 21:55:27 +0200

    deploying

--- src/main/java/com/michal/crm/service/Email/EmailServiceImpl.java
+++ src/main/java/com/michal/crm/service/Email/EmailServiceImpl.java
@@ -16,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import javax.mail.*;
@@ -28,7 +29,7 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Properties;
 
-@Service
+@Repository
 public class EmailServiceImpl implements EmailService{
 
     @Autowired
