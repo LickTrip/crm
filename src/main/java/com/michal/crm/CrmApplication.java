73e84e4be26f30387e9e715d0359efbad459eb4f
Michal <lick.trip@gmail.com>
2019-08-06 22:12:20 +0200

    deploying

--- src/main/java/com/michal/crm/CrmApplication.java
+++ src/main/java/com/michal/crm/CrmApplication.java
@@ -8,9 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableCaching
+@ComponentScan(basePackages = "com.michal.crm.service.Email")
 @EnableConfigurationProperties(StorageProperties.class)
 public class CrmApplication {
 	public static void main(String[] args) {
